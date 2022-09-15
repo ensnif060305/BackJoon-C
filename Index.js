cc4a4b5119e3d76f09336fbfa2249dd4b8bbbf24
Ensnif <101508194+ensnif060305@users.noreply.github.com>
2022-09-15 11:21:19 +0900

    backjoon-node.js 1000, 1001 update

--- Index.js
+++ Index.js
@@ -6,3 +6,23 @@ console.log("Hello World!");
 
 console.log(`강한친구 대한육군
 강한친구 대한육군`);
+
+/*1000*/
+
+const fs = require('fs');
+const inputData = fs.readFileSync(0, 'utf8').toString().split(' ');
+
+const A = parseInt(inputData[0]);
+const B = parseInt(inputData[1]);
+
+console.log(A+B);
+
+/*1001*/
+
+const fs = require('fs');
+const inputData = fs.readFileSync(0, 'utf8').toString().split(' ');
+
+const A = parseInt(inputData[0]);
+const B = parseInt(inputData[1]);
+
+console.log(A-B);
